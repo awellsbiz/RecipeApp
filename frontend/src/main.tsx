c94ba835a811dbbfcf9e6c650da36ba9838893ff
awellsbiz <awellsbiz@gmail.com>
2024-01-17 13:15:47 -0800

    uninstall clerk

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { ClerkProvider } from '@clerk/clerk-react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-<App />
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
